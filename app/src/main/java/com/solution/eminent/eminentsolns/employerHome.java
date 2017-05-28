4cc5236724633d0e26c5b07699c61e47b93ce5ed
Harshil Gupta <harshilgupta1808@gmail.com>
2017-05-28 16:29:32 +0530

    Basic Skeleton

--- app/src/main/java/com/solution/eminent/eminentsolns/employerHome.java
+++ app/src/main/java/com/solution/eminent/eminentsolns/employerHome.java
@@ -102,6 +102,6 @@ public class employerHome extends AppCompatActivity
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        return true;
+        return false;
     }
 }
